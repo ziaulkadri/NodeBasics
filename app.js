ea727589933250dd882bd9e807e86330ad616458
ziaulkadri <ziaul.kadri@innoage.in>
2023-02-02 10:47:43 +0530

    added routes

--- app.js
+++ app.js
@@ -1,33 +1,6 @@
 const http = require('http');
-const fs = require('fs');
-const server = http.createServer((req, res) => {
-	//console.log(req.url, req.method, req.headers);
-	//process.exit();
-	const url = req.url;
-	const method = req.method;
-	if (url === '/') {
-		res.write('<html>');
-		res.write('<head><title>Enter Message</title></head>');
-		res.write(
-			'<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
-		);
+const routes = require('./routes');
 
-		res.write('</html>');
-		return res.end();
-	}
-	if (url === '/message' && method === 'POST') {
-		fs.writeFileSync('message.txt', 'DUMMY');
-		res.statusCode = 302;
-		res.setHeader('Location', '/');
-		return res.end();
-	}
-	res.setHeader('Content-Type', 'text/html');
-	res.write('<html>');
-	res.write('<head><title>Hello World</title></head>');
-	res.write('<body><h1>Hello World from my node server</h1></body>');
-
-	res.write('</html>');
-	res.end();
-});
+const server = http.createServer(routes);
 
 server.listen(3000);
