8648cd39fc2b41e7eb476ecb4b26b041a16bd13a
ziaulkadri <ziaul.kadri@innoage.in>
2023-02-02 12:16:00 +0530

    added nodemon scripts

--- app.js
+++ app.js
@@ -2,5 +2,4 @@ const http = require('http');
 const routes = require('./routes');
 
 const server = http.createServer(routes);
-
 server.listen(3000);
--- routes.js
+++ routes.js
@@ -21,7 +21,7 @@ const requestHandler = (req, res) => {
 		});
 		return req.on('end', () => {
 			const parseBody = Buffer.concat(body).toString();
-			const message = parseBody.split('=')[1];
+			const message = parseBody.split('=')[0];
 			fs.writeFile('message.txt', message, (err) => {
 				res.statusCode = 302;
 				res.setHeader('Location', '/');
